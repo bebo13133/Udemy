39a86331aff8c4de5b26cac4e179ddf55edcb3ea
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-14 19:24:59 +0200

    add First project

---
+++ Vue - The Complete Guide/gs-01-starting-project/app.js
@@ -0,0 +1,16 @@
+
+
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const listEl= document.querySelector('ul');
+
+function onGoal() {
+    const enteredValue= inputEl.value
+    const listItemEl = document.createElement('li');
+
+    listItemEl.textContent = enteredValue
+    listEl.appendChild(listItemEl)
+    inputEl.value=""
+}
+
+buttonEl.addEventListener('click',onGoal)
