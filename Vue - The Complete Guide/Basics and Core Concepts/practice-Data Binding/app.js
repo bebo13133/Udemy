67c7c962d07ee68bb834636db7e4faad383ca0a6
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-15 20:47:54 +0200

    add methods

--- Vue - The Complete Guide/Basics and Core Concepts/practice-Data Binding/app.js
+++ Vue - The Complete Guide/Basics and Core Concepts/practice-Data Binding/app.js
@@ -4,6 +4,15 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods:{
+    onAdd (){
+      return this.counter++;
+    },
+    onReducer () {
+      return this.counter-=1
+
+    }
+  }
 });
 
 app.mount('#events');
