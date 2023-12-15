eecd471753b4e539c5842bdb57ea8e3d43bfee41
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-15 19:05:31 +0200

    адд

--- Vue - The Complete Guide/Basics and Core Concepts/basics-01-starting-code/app.js
+++ Vue - The Complete Guide/Basics and Core Concepts/basics-01-starting-code/app.js
@@ -1,10 +1,20 @@
 const app = Vue.createApp({
 
-    data(){
+    data() {
         return {
-            courseGoals:"Finish the Course and learn Vue",
+            courseGoals: "Finish the Course and learn Vue",
             vueLink: "https://vuejs.org/",
         };
+    },
+    methods: {
+        outputGoal() {
+            const mathRandomNumber = Math.random();
+            if (mathRandomNumber < 0.5) {
+                return "Learn more"
+            } else {
+                return "Master Vue"
+            }
+        }
     }
 
 })
