d1913abd17ad6adcdd3181426bd6b0fa483030f4
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-16 13:04:31 +0200

    add watch

--- Vue - The Complete Guide/Basics and Core Concepts/practice-part2-Data-Binding/app.js
+++ Vue - The Complete Guide/Basics and Core Concepts/practice-part2-Data-Binding/app.js
@@ -2,32 +2,43 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      fullName: '',
     };
   },
-  computed:{
- 
-    fullName (){
-      console.log("hi")
-      if(this.name===""){
-        return ''
+  watch: {
+    name(value) {
+      if (value === "") {
+        this.fullName = ''
+      } else {
+
+        this.fullName = value + ' ' + "Iliev"
       }
-      return this.name + ' ' + "Iliev"
-    } 
+    },
+  },
+  computed: {
+
+    fullName() {
+      // console.log("hi")
+      // if(this.name===""){
+      //   return ''
+      // }
+      // return this.name + ' ' + "Iliev"
+    }
   },
   methods: {
 
-    outputFullName (){
-    
+    outputFullName() {
+
 
     },
 
-    resetInput (){
+    resetInput() {
       this.name = ""
     },
 
     setName(event, lastName) {
-      this.name = event.target.value ;
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
