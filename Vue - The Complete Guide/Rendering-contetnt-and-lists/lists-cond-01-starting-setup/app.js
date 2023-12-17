7c3ddffb479d38d572fc6f3dda0ecfe7518d6c84
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-17 10:44:45 +0200

    add

--- Vue - The Complete Guide/Rendering-contetnt-and-lists/lists-cond-01-starting-setup/app.js
+++ Vue - The Complete Guide/Rendering-contetnt-and-lists/lists-cond-01-starting-setup/app.js
@@ -1,7 +1,16 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      addInputValue: '',
+      goals: []
+    };
   },
+  methods: {
+    addGoal() {
+      this.goals.push(this.addInputValue)
+      this.addInputValue = ''
+    },
+  }
 });
 
 app.mount('#user-goals');
