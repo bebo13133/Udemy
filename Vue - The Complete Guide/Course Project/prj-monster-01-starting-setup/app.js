c83553c49c901d2046a774acf1b3da702bf01391
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-18 16:38:55 +0200

    add special attack

--- Vue - The Complete Guide/Course Project/prj-monster-01-starting-setup/app.js
+++ Vue - The Complete Guide/Course Project/prj-monster-01-starting-setup/app.js
@@ -1,18 +1,44 @@
+const randomValue=(max,min)=>{
+    return  Math.floor(Math.random() * (max - min) + min);
+}
+
+
 const app = Vue.createApp({
     data() {
         return {
             playerHealth: 100,
             monsterHealth: 100,
+            currentRound: 0,
         }
+    },
+    computed:{
+        monsterHealthBar(){
+           return {width: this.monsterHealth + '%'}
+        },
+        playerHealthBar(){
+            return {width: this.playerHealth + '%'}
+         },
+         mayUseAttack (){
+            return this.currentRound % 3 !== 0
+         }
     },
     methods: {
         attackMonster() {
-            const attackValue = Math.floor(Math.random() * (12 - 5) - 5)
-            this.monsterHealth -= attackValue
+            this.currentRound++
+            const attackValue = randomValue(12,5);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer()
         },
         attackPlayer() {
-            const attackValue = Math.floor(Math.random() * (15 - 8) - 8)
-            this.playerHealth -= attackValue
+            const attackValue =  randomValue(15,8);
+            this.playerHealth -= attackValue;
+        },
+        specialAttack() {
+            this.currentRound++
+            const attackValue =  randomValue(25,10);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer()
+
         }
     }
 })
