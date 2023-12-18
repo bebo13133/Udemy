51f85700adda9c0ed3c65f1b24e51038314333bb
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-18 11:12:54 +0200

    ...

---
+++ Vue - The Complete Guide/Course Project/prj-monster-01-starting-setup/app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            playerHealth: 100,
+            monsterHealth: 100,
+        }
+    },
+    methods: {
+        attackMonster() {
+            const attackValue = Math.floor(Math.random() * (12 - 5) - 5)
+            this.monsterHealth -= attackValue
+        },
+        attackPlayer() {
+            const attackValue = Math.floor(Math.random() * (15 - 8) - 8)
+            this.playerHealth -= attackValue
+        }
+    }
+})
+
+app.mount("#game")
