7191a6f6097d60d042571bf7b5ae678ef8abce0f
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-18 16:55:01 +0200

    add
    heal button

--- Vue - The Complete Guide/Course Project/prj-monster-01-starting-setup/app.js
+++ Vue - The Complete Guide/Course Project/prj-monster-01-starting-setup/app.js
@@ -1,5 +1,5 @@
-const randomValue=(max,min)=>{
-    return  Math.floor(Math.random() * (max - min) + min);
+const randomValue = (max, min) => {
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
 
@@ -11,33 +11,40 @@ const app = Vue.createApp({
             currentRound: 0,
         }
     },
-    computed:{
-        monsterHealthBar(){
-           return {width: this.monsterHealth + '%'}
+    computed: {
+        monsterHealthBar() {
+            return { width: this.monsterHealth + '%' }
         },
-        playerHealthBar(){
-            return {width: this.playerHealth + '%'}
-         },
-         mayUseAttack (){
+        playerHealthBar() {
+            return { width: this.playerHealth + '%' }
+        },
+        mayUseAttack() {
             return this.currentRound % 3 !== 0
-         }
+        }
     },
     methods: {
         attackMonster() {
             this.currentRound++
-            const attackValue = randomValue(12,5);
+            const attackValue = randomValue(12, 5);
             this.monsterHealth -= attackValue;
             this.attackPlayer()
         },
         attackPlayer() {
-            const attackValue =  randomValue(15,8);
+            const attackValue = randomValue(15, 8);
             this.playerHealth -= attackValue;
         },
         specialAttack() {
             this.currentRound++
-            const attackValue =  randomValue(25,10);
+            const attackValue = randomValue(25, 10);
             this.monsterHealth -= attackValue;
             this.attackPlayer()
+
+        },
+        healPlayer() {
+            console.log("hi")
+            const healValue = randomValue(20, 8)
+
+           if (this.playerHealth + healValue > 100) { this.playerHealth = 100}else{ this.playerHealth += healValue}
 
         }
     }
