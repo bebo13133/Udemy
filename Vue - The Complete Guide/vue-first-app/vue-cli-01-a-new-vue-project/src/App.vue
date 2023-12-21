a843ebf7b0c4ce74659e829e166222b26a9e70e0
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-21 17:59:05 +0200

    add

---
+++ Vue - The Complete Guide/vue-first-app/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<!-- eslint-disable vue/valid-template-root -->
+<template>
+<section>
+<h2 id="">My friends</h2>
+<ul>
+    <li></li>
+</ul>
+</section>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenzo",
+          phone: "0894371779",
+          email: "bebo@abv.bg",
+        },
+        {
+          id: "john",
+          name: "John Petrov",
+          phone: "0894371779",
+          email: "john@abv.bg",
+        },
+      ],
+    };
+  },
+};
+</script>
