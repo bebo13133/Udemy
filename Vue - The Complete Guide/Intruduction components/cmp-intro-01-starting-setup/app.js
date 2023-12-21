69049dd980edc435b2a7e528905268614dcdf5f4
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-21 11:38:23 +0200

    create vue/cli

--- Vue - The Complete Guide/Intruduction components/cmp-intro-01-starting-setup/app.js
+++ Vue - The Complete Guide/Intruduction components/cmp-intro-01-starting-setup/app.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
     data(){
        return {
-        detailsAreVisible: false,
+        // detailsAreVisible: false,
         friends:[
             {id:'manuel', name:'Manuel Lorenz', phone:'01234 5678 991', email:'manuel@localhost.com'},
             {id:'julie', name:'Julie Jones', phone:'Julie Jones', email:'julie@localhost.com'}
@@ -10,10 +10,31 @@ const app = Vue.createApp({
     }
     },
     methods: {
-        toggleDetails (){
-            this.detailsAreVisible= !this.detailsAreVisible
-        }
+    
     }
 })
+app.component('friends-contacts', {
+    template:`
+    <li >
+    <h2>{{friend.name}}</h2>
+    <button @click="toggleDetails" >{{detailsAreVisible ? 'Hide':'Show'}}</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{friend.phone}}</li>
+      <li><strong>Email:</strong> {{friend.email}}</li>
+    </ul>
+  </li>
+    `,
+data(){
+    return {
+        detailsAreVisible: false,
+        friend:{id:'manuel', name:'Manuel Lorenz', phone:'01234 5678 991', email:'manuel@localhost.com'}
+    }
+},
+methods:{
+    toggleDetails (){
+        this.detailsAreVisible= !this.detailsAreVisible
+    }
+}
 
+})
 app.mount('#app')
