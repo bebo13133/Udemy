e23147f2e39be1e44fbe33b568b737018e628e6e
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-22 12:14:21 +0200

    push

---
+++ Vue - The Complete Guide/vue-first-app/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<!-- eslint-disable vue/valid-template-root -->
+
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetailsVisible">Show</button>
+    <ul v-if="detailsVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      detailsVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenzo",
+        phone: "0894371779",
+        email: "bebo@abv.bg",
+      },
+    };
+  },
+  methods:{
+    toggleDetailsVisible(){
+        this.detailsVisible = !this.detailsVisible;
+    }
+  }
+};
+</script>
