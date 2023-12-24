21189f8b1be7adad1770332fc5c67a6d91f3ff77
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-24 12:54:45 +0200

    finish

--- Vue - The Complete Guide/Practice - Props and custom events/cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeBase.vue
+++ Vue - The Complete Guide/Practice - Props and custom events/cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeBase.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <h2>Select a Topic</h2>
-    <knowledge-grid @select-topic="$emit('select-topic', $event)"></knowledge-grid>
+    <knowledge-grid ></knowledge-grid>
   </section>
 </template>
 
--- Vue - The Complete Guide/Practice - Props and custom events/cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeGrid.vue
+++ Vue - The Complete Guide/Practice - Props and custom events/cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeGrid.vue
@@ -6,7 +6,7 @@
       :id="topic.id"
       :topic-name="topic.title"
       :description="topic.description"
-      @select-topic="$emit('select-topic', $event)"
+      
     ></knowledge-element>
   </ul>
 </template>
