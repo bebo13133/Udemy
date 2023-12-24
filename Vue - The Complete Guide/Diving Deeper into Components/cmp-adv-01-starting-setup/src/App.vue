5999fa677cda1816f0b36ff085621d7d253088db
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-24 18:12:29 +0200

    add

--- Vue - The Complete Guide/Diving Deeper into Components/cmp-adv-01-starting-setup/src/App.vue
+++ Vue - The Complete Guide/Diving Deeper into Components/cmp-adv-01-starting-setup/src/App.vue
@@ -11,7 +11,13 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+
+
 export default {
+  components: {
+    'the-header': TheHeader,
+    },
   data() {
     return {
       activeUser: {
