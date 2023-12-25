f827302f0fd97378b63a1004f22583c7fcecb93f
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-25 16:18:24 +0200

    Update App.vue

--- Vue - The Complete Guide/Diving Deeper into Components/cmp-adv-01-starting-setup/src/App.vue
+++ Vue - The Complete Guide/Diving Deeper into Components/cmp-adv-01-starting-setup/src/App.vue
@@ -16,7 +16,7 @@ import TheHeader from './components/TheHeader.vue';
 
 export default {
   components: {
-    'the-header':  TheHeader,
+    'the-header': TheHeader,
     },
   data() {
     return {
