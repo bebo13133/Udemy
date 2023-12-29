fe67811d28cff02c6bce93974cdf5377701594c3
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-29 09:01:23 +0200

    add

--- Vue - The Complete Guide/Diving Deeper into Components/cmp-adv-01-starting-setup/src/App.vue
+++ Vue - The Complete Guide/Diving Deeper into Components/cmp-adv-01-starting-setup/src/App.vue
@@ -7,6 +7,9 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     />
+    <course-goals>
+      <h2></h2>
+    </course-goals>
   </div>
 </template>
 
@@ -15,12 +18,14 @@ import TheHeader from './components/TheHeader.vue';
 import UserInfo from './components/UserInfo.vue';
 import BadgeList from './components/BadgeList.vue';
 
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
   components: {
     TheHeader,
     UserInfo,
-    BadgeList
+    BadgeList,
+    CourseGoals
     },
   data() {
     return {
--- Vue - The Complete Guide/Diving Deeper into Components/cmp-adv-01-starting-setup/src/components/CourseGoals.vue
+++ Vue - The Complete Guide/Diving Deeper into Components/cmp-adv-01-starting-setup/src/components/CourseGoals.vue
@@ -1,11 +1,18 @@
 <template>
-    <ul></ul>
+    <ul>
+<li v-for="goal in goals" :key="goal">
+    <slot></slot>
+</li>
+
+    </ul>
 </template>
 
 <script>
 export default {
     data() {
-        
+        return{
+            goals:['Finish the course', 'Learned Vue']
+        }
     },
 }
 </script>
