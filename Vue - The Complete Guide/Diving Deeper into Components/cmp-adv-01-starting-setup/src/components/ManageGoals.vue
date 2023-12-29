2b8730bbb714539e6a1e61d2023442b5c7e1367a
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-29 14:12:12 +0200

    Update ManageGoals.vue

--- Vue - The Complete Guide/Diving Deeper into Components/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ Vue - The Complete Guide/Diving Deeper into Components/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,37 +1,37 @@
 <template>
-<div>
+  <div>
     <h2>Manage goals</h2>
-    <input type="text" ref="goal"/>
+    <input type="text" ref="goal" />
     <button @click="setGoal">Set Goal</button>
-    <error-alert v-if="isInputInvalid">
+    <teleport to="body">
+      <error-alert v-if="isInputInvalid">
         <h2>Input is invalid</h2>
         <p>Please entered a characters</p>
-    <button @click="ErrorOkay">Okay</button>
-    </error-alert>
-</div>
+        <button @click="ErrorOkay">Okay</button>
+      </error-alert>
+    </teleport>
+  </div>
 </template>
 <script>
-import ErrorAlert from './ErrorAlert.vue'
+import ErrorAlert from "./ErrorAlert.vue";
 
 export default {
-    components: {
-   ErrorAlert,
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      isInputInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredGoal = this.$refs.goal.value;
+      if (enteredGoal === "") this.isInputInvalid = true;
     },
-    data() {
-        return {
- 
-        isInputInvalid: false
-
-        }
+    ErrorOkay() {
+      this.isInputInvalid = false;
     },
-    methods: {
-        setGoal(){
-         const enteredGoal =this.$refs.goal.value
-         if(enteredGoal === '') this.isInputInvalid = true
-         },
-         ErrorOkay (){
-            this.isInputInvalid = false
-         }
-    },
-}
+  },
+};
 </script>
